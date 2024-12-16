116c5d84a6ba2c0dc081240ada60f539d3c2cab2
Satyaprakash Roy <149265635+SatyaT10@users.noreply.github.com>
2024-12-16 14:50:00 +0530

    Delete Server directory

--- Server/Route/routes.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const { newUser,mainPage} = require('../Controllers/controllers');
-
-router.post('/feedback',newUser)
-router.post('/mainpage',mainPage)
-
-
-
-module.exports = router
